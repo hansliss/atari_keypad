f7f70684936c0277c1a2e331f037c639cbbbdae5
Hans Liss <Hans@Liss.pp.se>
2021-01-26 16:06:33 +0100

    Added comments to assembler code

--- VIC-20/kppoll.asm
+++ VIC-20/kppoll.asm
@@ -1,31 +1,37 @@
 * = $1A00
 
-; bit organization
+; bits within the two bytes
+; and their correspondence to
+; keypad keys
 ;     012   byte 0
 ;     345
-;     678
-;     9ab   byte 1 from "8"
+;     670
+;     123   byte 1
 
-; scratch storage
 ;  idx of byte
-;  bitmask
-;  inversebitmask
-;  row
-;  column 
-;  columns (all column bits)
 IDX     BYTE 0
+;  bitmask
 BITMASK BYTE 0
+;  inversebitmask
 INVBM   BYTE 0
+;  row
 ROW     BYTE 0
+;  column 
 COLUMN  BYTE 0
+;  columns (all column bits)
 COLUMNS BYTE 0
+;  saved data direction registers
 V1DDRA  BYTE 0
 V2DDRB  BYTE 0
+;  scratch storage
 SCRATCH BYTE 0
 
 ; persistent
+;  the two key state bytes
 KEY     BYTE 0,0
+;  the previous state of keys
 LAST    BYTE 0,0
+;  debouncing status
 DEBST   BYTE 0,0
 
         SEI
@@ -34,16 +40,18 @@ DEBST   BYTE 0,0
         STA V1DDRA
         LDA $9122
         STA V2DDRB
-        ; row=0
+        ; Loop over the row number, high to low
         LDA #$03
         STA ROW
         ; set output on 1<<row, input on the others
-        ; pull low 1<<row
+        ; pull the output low
 RLOOP
         LDY ROW
         TYA
         CMP #$03
         BEQ RSPEC
+        ; Rows 0 to 2 are connected to adjacent bits
+        ; on VIA 1 PORT A, from bit 2 to bit 4
         LDA #$02
 RSHFT1
         ROL
@@ -51,17 +59,25 @@ RSHFT1
         BPL RSHFT1
         STA $9113
         LDA #$00
+        ; Reset the other VIA port to input
+        ; so it doesn't interfere!
         STA $9122
+        ; Set the output to low
         STA $9111
         BEQ RDONE
+        ; Row 3 is different, it's connected to
+        ; VIA 2 port B
 RSPEC
         LDA #$80
         STA $9122
         LDA #$00
+        ; Reset the other VIA port to input
+        ; so it doesn't interfere!
         STA $9113
+        ; Set the output to low
         STA $9120
 RDONE
-        ; wait
+        ; wait for a bit
         LDY #$80
 DELAY
         NOP
@@ -95,11 +111,9 @@ C4
         STA COLUMN
 
 CLOOP
-        ; set idx, shift and bitmask
+        ; Now loop over the columns
+        ; calculate idx, shift and bitmask
         ; idx = (row & 0x01 | column >> 1) & row >> 1
-        ; calculate idx
-        ; y = row
-        ; a = column
         LDA COLUMN
         LDY ROW
         LSR
@@ -113,7 +127,6 @@ CLOOP
         AND SCRATCH
         STA IDX
         ; shift = (3 * row + column) % 8
-        ; calculate shift
         ; y = row
         TYA
         CLC
@@ -146,18 +159,24 @@ CBLOOP
         ROL
         DEY
         BPL CBLOOP
-;       STA COLBITMASK
+        ; We don't need to store this bitmask
 
         LDY IDX
-        ; if !(columns & colbitmask) goto isprsd  // <<<---- need "columns" here only
+        ; if !(columns & colbitmask) goto isprsd
+        ; We only use COLUMNS and the colbitmask here,
+        ; so there might be room for optimization.
         AND COLUMNS
         BEQ ISPRSD
         
+        ; Here if the current key is *not* pressed.
+        ; Check whether it was pressed the last time.
         ; if !(last[idx] & bitmask) goto end
         LDA LAST,Y
         AND BITMASK
         BEQ END
 
+        ; Release event - we just clear the current bit
+        ; in KEY, LAST and DEBST
         LDX INVBM
         ; key[idx] &= inversebitmask
         TXA
@@ -175,16 +194,19 @@ CBLOOP
 ISPRSD
         LDX BITMASK
 
+        ; Key is pressed. Was it already pressed?
         ; if last[idx] & bitmask goto end
         LDA LAST,Y
         AND BITMASK
         BNE END
 
+        ; Nope, check whether it has been debounced
         ; if !(debounce[idx] & bitmask) goto deb
         LDA DEBST,Y
         AND BITMASK
         BEQ DEB
 
+        ; Now set the bit in KEY and LAST
         ; key[idx] |= bitmask
         TXA
         ORA KEY,Y
@@ -194,6 +216,9 @@ ISPRSD
         ORA LAST,Y
         STA LAST,Y
         JMP END
+
+        ; Debounce - just mark this as on in DEBST
+        ; and we will handle it on the next run
 DEB
         ; debounce[idx] |= bitmask
         TXA
